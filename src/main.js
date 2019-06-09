e336e47845ef5888243630db2d59fbfc8c27c20e
happystudy666 <50537766+happystudy666@users.noreply.github.com>
2019-06-09 11:46:58 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
-import ElementUI from 'element-ui'
-import "element-ui/lib/theme-chalk/index.css"
+import store from './store'
+import elementui from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import '@/assets/icon_font/iconfont.css'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
-
-/* eslint-disable no-new */
+Vue.use(elementui)
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
